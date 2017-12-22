7f5602f95cc17c1faec0cb158a2dc5bd13ed675b
Tom Medema <tommedema@gmail.com>
2017-12-23 01:35:21 +0800

    semicolon

--- src/css.js
+++ src/css.js
@@ -39,7 +39,7 @@ module.exports = function( options, callback )
 
     var rebase = function( src )
     {
-        var resolved = path.resolve( '/', settings.rebaseRelativeTo, src ).substring( 1 )
+        var resolved = path.resolve( '/', settings.rebaseRelativeTo, src ).substring( 1 );
         var css = "url(\"" + resolved.replace( /\\/g, "/" ) + "\")";
         var re = new RegExp( "url\\(\\s?[\"']?(" + inline.escapeSpecialChars( src ) + ")[\"']?\\s?\\)", "g" );
         result = result.replace( re, () => css );
